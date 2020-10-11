367d9ed51ae97e664432a8e86d0285a4b7675718
pei <muhammadsyafei0404@gmail.com>
2020-10-11 21:24:54 +0700

    first commit

---
+++ app/src/main/java/com/stematel/pei/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.stematel.pei.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stematel.pei.myflexiblefragment.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
